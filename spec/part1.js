60010ca40530b8996334fadc808b02c86c4ca21a
jgenso1690 <jimenagensollen@gmail.com>
2020-03-08 23:13:39 -0400

    moved var unique object

--- spec/part1.js
+++ spec/part1.js
@@ -8,9 +8,9 @@
       checkForNativeMethods(function() {
         _.identity(1);
       });
-
-      it('should return whatever value is passed into it', function() {
         var uniqueObject = {};
+      it('should return whatever value is passed into it', function() {
+        
         expect(_.identity(1)).to.equal(1);
         expect(_.identity('string')).to.equal('string');
         expect(_.identity(false)).to.equal(false);
