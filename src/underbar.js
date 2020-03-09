84556796c94d8b5c040844df1291a0af1b73e20f
jgenso1690 <jimenagensollen@gmail.com>
2020-03-09 00:12:42 -0400

    test3 typo

--- src/underbar.js
+++ src/underbar.js
@@ -61,14 +61,17 @@
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
     if (Array.isArray(collection)){
+      
       for (let i = 0 ; i< collection.length ; i++){
         iterator(collection[i], i, collection);
-      }else {
+      }
+    }
+      else {
         for ( let key in collection){
             iterator(collection[key], key, collection)
         }
       }
-    }
+    
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
