f466b5bb6d767cf1cece1ca247ea78fabe6142cb
jgenso1690 <jimenagensollen@gmail.com>
2020-03-10 22:22:07 -0400

    test9  uniq

--- src/underbar.js
+++ src/underbar.js
@@ -120,21 +120,14 @@
     
     var uniqele=[];
 
-    if (isSorted === undefined){
-      for (var i=0; i< array.length;i++){
-      if (!uniqele.includes(array[i])){
-        uniqele.push(array[i]);
-      
-       }else{
-        var sorted = array.sort();
+    var sorted = array.slice().sort
+        
          for (var i=0; i< sorted.length;i++){
            if (!uniqele.includes(sorted[i])){
              uniqele.push(sorted[i]);
            }
           }
-       }
-      }
-    }
+       
     return uniqele
   
   };
