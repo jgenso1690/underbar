58e873ddc1bc512efe7b35a8b745469559b649cc
jgenso1690 <jimenagensollen@gmail.com>
2020-03-13 13:09:30 -0400

    test1 some

--- src/underbar.js
+++ src/underbar.js
@@ -296,6 +296,13 @@
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
   
+     if (Array.isArray(collection)){
+      for (let i = 0; i< collection.length ; i++){
+        if (iterator(collection[i]){
+          return true;
+        }
+      }
+     }return false
   };
 
 
