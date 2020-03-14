f026de40014bcb7c56bfed000b7a53d8480c004e
jgenso1690 <jimenagensollen@gmail.com>
2020-03-13 22:07:30 -0400

    test extend

--- src/underbar.js
+++ src/underbar.js
@@ -324,8 +324,8 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-    var source = _.extend.arguments[1];
-    
+    var source = function.arguments[1];
+
     for (var keys in obj){
       for (var otherkeys in source){
         obj[otherkeys] = source[otherkeys];
